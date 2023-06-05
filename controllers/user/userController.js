fb90d41058ba4777a4be0d850487a6c26f56262d
pallab99 <kitkatpallab@gmail.com>
2023-06-05 16:00:45 +0600

    User registration is added

---
+++ controllers/user/userController.js
@@ -0,0 +1,25 @@
+import { errorHandler } from '../../middlewares/error.js';
+import { User } from '../../model/user/userModel.js';
+import bcrypt from 'bcrypt';
+
+export const registerNewUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    let userName = await User.findOne({ name });
+    if (userName) {
+      res.status(400).json(errorHandler('Name is already taken'));
+    }
+    if (user) {
+      res.status(400).json(errorHandler('Email is already taken'));
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({ name, email, password: hashedPassword });
+    res.json({
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
