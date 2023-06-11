b999e12a7a1a52adcba03d9c54b1e56cad83186c
pallab99 <kitkatpallab@gmail.com>
2023-06-11 12:57:18 +0600

    expire verification code after 2 mins and resent verification code feature is added

--- helper/randomNumberGenerator.js
+++ helper/randomNumberGenerator.js
@@ -3,3 +3,7 @@ export const generateRandomNumber = () => {
   const maxm = 999999;
   return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
 };
+
+export const isVerificationCodeExpired = (user) => {
+  return new Date() > user.verificationCodeExpiresAt;
+};
